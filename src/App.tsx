a62cafc80ecfa80d438f9fc1257bb263c11a26ca
ttooeasycraft-ui <ttooeasycraft@gmail.com>
2026-06-15 17:06:01 -0300

    feat(nav): add Radar do Bot nav item and route for EzBot_IA

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Shop from "@/pages/Shop";
 import HowToJoin from "@/pages/HowToJoin";
 import Radar from "@/pages/Radar";
 import Radar from "@/pages/Radar";
+import Radar from "@/pages/Radar";
 import {
   Home as HomeIcon,
   BookOpen,
